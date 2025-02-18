d303191ef5b6b596bac2111e89bb1728551218e8
Anna Gubina <anya.gubina101@gmail.com>
2025-02-18 13:51:22 -0500

    Первая попытка сдачи дз

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let Discr = (Math.pow(b, 2) - (4 * a * c));
+
+  if(Discr < 0) {
+    return arr;
+  }
+   if (Discr == 0) {
+    let x = (- b / 2 * a);
+
+    arr.push(x);
+  } else if (Discr > 0) {
+    let x1 = ((- b + Math.sqrt(Discr)) / (2 * a));
+    let x2 = ((- b - Math.sqrt(Discr)) / (2 * a));
+
+    arr.push(x1, x2);
+  }
+
+  console.log(arr);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (monthlyRate + (monthlyRate / (Math.pow((1 + monthlyRate), countMonths) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+
+  return +totalPayment.toFixed(2);
 }
