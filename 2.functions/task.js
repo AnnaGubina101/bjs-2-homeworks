56edec353acb4e0d65141182274c7d754cd3fbc2
Anna Gubina <anya.gubina101@gmail.com>
2025-02-26 17:50:15 -0500

    Третья доработка третьего задания

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -36,7 +36,7 @@ function differenceMaxMinWorker(...arr) {
   for(let i = 0; i < arr.length; i++) {
     if(arr[i] > max) {
       max = arr[i];
-    } else if(arr[i] < max) {
+    } else if(arr[i] < min) {
       min = arr[i];
     }
   }
