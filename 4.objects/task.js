b72b9032a3fb2741cd16c2c2366e85e15f4cb1c7
Anna Gubina <anya.gubina101@gmail.com>
2025-03-05 17:04:20 -0500

    Первая сдача четвертого задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.marks === undefined){
+        return marks = 0 
+    } else {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.marks === undefined || null) {
+    return 0 
+  } else {
+    let sum = this.marks.reduce(function(a, b) {
+        return a + b;
+    }, 0)
+    let total = sum / this.marks.length;
+    return total
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject
+  delete this.marks
+  this.excluded = reason;
 }
