09a2009fe1f1ae34f4582f140cfd4e95bb1315c0
Anna Gubina <anya.gubina101@gmail.com>
2025-03-17 14:04:39 -0400

    Первая сдача дз по классам и обработки исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,45 @@
-﻿
+﻿function parseCount(a) {
+    let result = Number.parseFloat(a);
+    if(Number.isNaN(result)) {
+         throw new Error("Невалидное значение");
+     }
+    return result;
+}
+
+function validateCount(a) {
+  try {
+    return parseCount(a);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if(this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        return (this.a + this.b + this.c);
+    }
+    get area() {
+        let halfP = 0.5 * (this.a + this.b + this.c);
+        let s = Math.sqrt(halfP * (halfP - this.a) * (halfP - this.b) * (halfP - this.c));
+        return s.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() { return new Error("Ошибка! Треугольник не существует") },
+            get area() { return new Error("Ошибка! Треугольник не существует") }
+        }
+        }
+    }
